ec12fc9b9a7e5673012be5abe8853d584dbb14e2
SampetovaN <nikasampetova@mail.ru>
2024-06-27 21:55:45 +0300

    Initial commit

---
+++ bot.js
@@ -0,0 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '7241986505:AAEQcYVYC7FORLH8FtHMtuKE5xBGWoN37ww';
+const bot = new TelegramBot(token, { polling: true });
+
+// Обработчик команды /start
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Добро пожаловать! Я ваш бот.');
+});
+
+// Обработчик текстовых сообщений
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Вы написали: ' + msg.text);
+});
