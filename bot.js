63af9d4f912e41621f32efd7873176f33eb6bed0
SampetovaN <nikasampetova@mail.ru>
2024-06-27 22:17:32 +0300

    add buttons

--- bot.js
+++ bot.js
@@ -2,15 +2,30 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '7241986505:AAEQcYVYC7FORLH8FtHMtuKE5xBGWoN37ww';
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработчик команды /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Добро пожаловать! Я ваш бот.');
+    const replyMarkup = {
+        reply_markup: {
+            keyboard: [
+                [{ text: 'Кнопка 1' }, { text: 'Кнопка 2' }],
+                [{ text: 'Кнопка 3' }]
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true
+        }
+    };
+    bot.sendMessage(chatId, 'Выберите опцию:', replyMarkup);
 });
 
-// Обработчик текстовых сообщений
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    console.log('fffg', 'sssss')
-    bot.sendMessage(chatId, 'Вы написали: ' + msg.text);
+    if (msg.text === 'Кнопка 1') {
+        bot.sendMessage(chatId, 'Вы нажали на Кнопку 1');
+    } else if (msg.text === 'Кнопка 2') {
+        bot.sendMessage(chatId, 'Вы нажали на Кнопку 2');
+    } else if (msg.text === 'Кнопка 3') {
+        bot.sendMessage(chatId, 'Вы нажали на Кнопку 3');
+    } else {
+        bot.sendMessage(chatId, 'Неизвестная команда');
+    }
 });
